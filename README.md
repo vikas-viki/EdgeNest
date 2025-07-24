d14dbd7f179010860a6965e249b1926489a3b4ff
vikas-viki <viki57394@gmail.com>
2025-07-24 12:40:29 +0530

    vercel clone v1 complete

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # EdgeNest
-vercel clone
 
-use `aws ecr get-login-password  --region ap-south-1 | docker login --username AWS --password-stdin 979188187252.dkr.ecr.ap-south-1.amazonaws.com/edgenest-builder` to login to docker as aws ecr user
+## flow V1
+- User makes request to the api server with github URL
+  - sends a unique projectID and projectURL specfic to user project
+  - api server creates a TASK on ECS 
+    - ECS uses the docker image (built from ./build-server) from ECR
+    - builds the projects
+    - along with building, it produces(pushes/sends) build logs using Kafka
+    - pushes /dist folder contents to S3 (with unique slug)
+- once user makes call to api server with github URL
+  - he/she's auto connected to websocket server 
+  - it starts consuming(listening) to the messages produced(sent) by kafka in build step and are shown to the user.
+- once build completes, user can visit to the URL 
+  - the URL is a sub-domain of the proxy server (GET /).
+  - it proxies the requests to S3 as user directly and serves the html file.
--- build-server/script.js
+++ build-server/script.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mimetype = require("mime-types");
 const dotenv = require("dotenv");
+const { Kafka } = require("kafkajs");
 dotenv.config();
 
 const s3Client = new S3Client({
@@ -13,47 +14,77 @@ const s3Client = new S3Client({
         secretAccessKey: process.env.S3_SECRET_KEY.toString()
     }
 });
+
+const kafkaClient = new Kafka({
+    clientId: 'edge-nest',
+    brokers: [process.env.KAFKA_SERVICE_URL.toString()],
+    ssl: true,
+    sasl:{
+        mechanism: 'plain',
+        username: process.env.KAFKA_USERNAME,
+        password: process.env.KAFKA_PASSWORD
+    }
+});
+const kafkaProducer = kafkaClient.producer();
 const projectID = process.env.PROJECT_ID;
+
+let key = 0;
+async function publishLog(log) {
+    await kafkaProducer.connect();
+    try {
+        await kafkaProducer.send({
+            topic: `edgenest-projectbulid-logs`,
+            messages: [
+                { key: `log---${projectID}---${key++}`, value: log.toString() }
+            ]
+        });
+    } catch (e) {
+        console.log("kafka error: ", e);
+    }
+}
+
 
 async function init() {
     console.log("executing script.js");
-
+    publishLog("Build Started...");
     const outputDir = path.join(__dirname, "output");
 
     const p = exec(`cd ${outputDir} && npm install && npm run build`);
 
     p.stdout.on("data", (data) => {
         console.log("data::", data.toString());
+        publishLog(data.toString());
     });
 
     p.stderr.on("data", (err) => {
         console.log("error: ", err.toString());
+        publishLog(`error: ${err.toString()}`);
     });
 
     p.on("close", async () => {
         console.log("Build completed!");
-
+        publishLog("Build completed.");
         const distFolderPath = path.join(__dirname, "output", "dist");
         const distFolderPathContent = fs.readdirSync(distFolderPath, { recursive: true });
-
+        publishLog("Uploading started");
         for (const filePath of distFolderPathContent) {
             const absPath = path.join(distFolderPath, filePath);
             if (fs.lstatSync(absPath).isDirectory()) continue;
 
             console.log(`uploading: ${filePath}`);
-
+            publishLog(`Uploading ${filePath}`);
             const cmd = new PutObjectCommand({
                 Bucket: 'edgenest-output',
                 Key: `__outputs/${projectID}/${filePath}`,
                 Body: fs.createReadStream(absPath),
-                ContentType: mimetype.lookup(filePath)  || 'application/octet-stream',
+                ContentType: mimetype.lookup(filePath) || 'application/octet-stream',
             });
 
             await s3Client.send(cmd);
 
             console.log("uploaded: ", filePath);
         }
-
+        publishLog("Upload complete!");
     })
 }
 init();
