fbd5b7b41d4b0d1a16814e178d9aff750ea44862
vikas-viki <viki57394@gmail.com>
2025-07-24 12:27:24 +0530

    able to get build logs from ecs tasks using kafka+socketio

--- api-server/index.js
+++ api-server/index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const { generateSlug } = require("random-word-slugs");
 const { RunTaskCommand, ECSClient } = require("@aws-sdk/client-ecs");
+const { } = require("socket.io");
+const { Kafka } = require('kafkajs');
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+
+const kafkaClient = new Kafka({
+    clientId: 'edge-nest',
+    brokers: [process.env.KAFKA_SERVICE_URL.toString()],
+    ssl: true,
+    sasl: {
+        mechanism: 'plain',
+        username: process.env.KAFKA_USERNAME,
+        password: process.env.KAFKA_PASSWORD
+    }
+});
+const kafkaConsumer = kafkaClient.consumer({ groupId: 'edgenest-logs-consumers' });
+
+const io = new Server({ cors: 'enable' });
+io.listen(9001, () => {
+    console.log("Socket server running  on: 9001");
+});
+io.on("connection", (socket) => {
+    socket.on('subscribe', (channel) => {
+        socket.join(channel);
+        socket.emit(`message`, `Joined ${channel}`);
+    })
+})
+
 
 const ecsClient = new ECSClient({
     region: 'ap-south-1',
@@ -19,7 +46,7 @@ const config = {
     TASK: 'arn:aws:ecs:ap-south-1:979188187252:task-definition/edgenest-builder-task'
 }
 
-app.post("/project",async (req, res) => {
+app.post("/project", async (req, res) => {
     const { gitURL } = req.body;
     const projectSlug = generateSlug();
 
@@ -47,7 +74,19 @@ app.post("/project",async (req, res) => {
                         {
                             name: 'GIT_REPOSITORY_URL',
                             value: gitURL
-                        }
+                        },
+                        {
+                            name: 'KAFKA_SERVICE_URL',
+                            value: process.env.KAFKA_SERVICE_URL.toString()
+                        },
+                        {
+                            name: 'KAFKA_USERNAME',
+                            value: process.env.KAFKA_USERNAME.toString()
+                        },
+                        {
+                            name: 'KAFKA_PASSWORD',
+                            value: process.env.KAFKA_PASSWORD.toString()
+                        },
                     ]
                 }
             ]
@@ -55,9 +94,20 @@ app.post("/project",async (req, res) => {
     });
 
     await ecsClient.send(cmd);
-    return res.status(200).json({status: 'queued', slug: projectSlug, url: `http://${projectSlug}.localhost:3000/`})
+    return res.status(200).json({ status: 'queued', slug: projectSlug, url: `http://${projectSlug}.localhost:3000/` })
 });
 
+async function initKafkaSubcribe() {
+    await kafkaConsumer.connect();
+    await kafkaConsumer.subscribe({ topic: 'edgenest-projectbulid-logs', fromBeginning: true });
+    await kafkaConsumer.run({
+        eachMessage: async ({ topic, message }) => {
+            const projectId = message.key.toString().split("---")[1];
+            io.to(projectId).emit('message', message.value.toString());
+        }
+    })
+}
+initKafkaSubcribe();
 app.listen(9000, () => {
     console.log("Api server running on port 9000!");
 })
