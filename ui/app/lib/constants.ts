4ece60a400aaad9f21fb379c2e39eddf16453975
vikas-viki <viki57394@gmail.com>
2025-07-31 10:50:41 +0530

    environment updated

--- ui/app/lib/constants.ts
+++ ui/app/lib/constants.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 export const SERVER_URL = process.env.SERVER_URL || "https://edgenest.onrender.com";
-export const WS_URL = `${process.env.SERVER_URL}/private`;
-export const WS_PUBLIC_URL = `${process.env.SERVER_URL}/public`;
+export const WS_URL = `${SERVER_URL}/private`;
+export const WS_PUBLIC_URL = `${SERVER_URL}/public`;
 export const SITE_URL = process.env.PROXY_SERVER || "https://edgenest-proxy.onrender.com";
-
-console.log({SERVER_URL})
 
 export const api = axios.create({
     baseURL: SERVER_URL?.toString(),
