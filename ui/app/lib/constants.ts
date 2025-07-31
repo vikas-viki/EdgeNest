198ab01b4217863a691296acd60cfa2f1af3f530
vikas-viki <viki57394@gmail.com>
2025-07-31 10:35:44 +0530

    environment updated

--- ui/app/lib/constants.ts
+++ ui/app/lib/constants.ts
@@ -5,7 +5,9 @@ export const WS_URL = `${process.env.SERVER_URL}/private`;
 export const WS_PUBLIC_URL = `${process.env.SERVER_URL}/public`;
 export const SITE_URL = process.env.PROXY_SERVER;
 
+console.log({SERVER_URL})
+
 export const api = axios.create({
-    baseURL: SERVER_URL,
+    baseURL: SERVER_URL?.toString(),
     withCredentials: true
 })
